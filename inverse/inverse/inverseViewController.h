ea3453d2255d4fda6a651642258a7e1e4339d8cd
emiru512 <1310034@nagoya-bunri.ac.jp>
2013-11-21 21:58:58 +0900

    キーボードを出したりしまったり

--- inverse/inverse/inverseViewController.h
+++ inverse/inverse/inverseViewController.h
@@ -8,7 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface inverseViewController : UIViewController
+
+//
+
+#define LOG(A, ...) NSLog(@"DEBUG: %s:%dline:%@", __PRETTY_FUNCTION__,__LINE__,[NSString stringWithFormat:A, ## __VA_ARGS__]);
+
+#define SCREEN ([[UIScreen mainScreen] bounds].size)
+#define STATUSBARHEIGHT (SCREEN.height - [[UIScreen mainScreen] applicationFrame].size.height)
+#define IOS_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
+
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_RETINA ([[UIScreen mainScreen] respondsToSelector:@selector(scale)] && [UIScreen mainScreen].scale > 1)
+
+//
+
+@interface inverseViewController : UIViewController <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) IBOutlet UITextField *urlTextField;
+
 
 @end
--- inverse/inverse/inverseViewController.m
+++ inverse/inverse/inverseViewController.m
@@ -19,14 +19,26 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    // 初期URL
     NSURL *url = [NSURL URLWithString:@"http://www.google.co.jp"];
     [_webView loadRequest:[NSURLRequest requestWithURL:url]];
+    
+    
+    // テキストフィールドのdelegateを設定
+    _urlTextField.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    // キーボードを隠す
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
